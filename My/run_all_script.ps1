55d6ad3c5a5d80642dfe6d426c81525e940bda4e
unknown <jack29817578@gmail.com>
2025-11-17 21:14:42 +0800

    fix bug

--- My/run_all_script.ps1
+++ My/run_all_script.ps1
@@ -15,4 +15,4 @@ Write-Host "[3/4] Running UM_DSI_DB_reverse experiments..."
 Write-Host "[4/4] Running MCSL experiments..."
 .\MCSL\run_script_MCSL.ps1
 
-Write-Host "[Parent] Child Script 已執行完畢。"
+Write-Host "[Parent] Child Script done。"
